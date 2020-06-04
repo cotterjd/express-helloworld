b6be310d09225bba5923f3b40ccce5728a4a02dc
Jordan Cotter <jrdn.cttr@gmail.com>
2020-06-04 15:39:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const { log } = console
 , bodyParser = require('body-parser')
 ;
 
-app.set('port', 3000)
 app.use(bodyParser.json()) // to be able to receive json in requests
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+app.set('port', 3000)
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
